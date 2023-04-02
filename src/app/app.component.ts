862ccb864b2e964edccad3bc87af3b52f844acb1
aghiljv <aghiljv@gmail.com>
2023-04-02 23:03:17 +0200

    basic styles added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-sorting-pipe';
+  options = [
+    { name: 'B', price: 5, startDate: new Date('2023-04-02T20:12:13.405Z') },
+    { name: 'A', price: 10, startDate: new Date('2023-04-05T20:12:13.405Z') },
+    { name: 'C', price: 3, startDate: new Date('2023-04-01T20:12:13.405Z') },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SortPipe } from './sort.pipe';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, SortPipe],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
